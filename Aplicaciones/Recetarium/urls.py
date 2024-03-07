f2408d09730f1e3b3db98feefb8d9dbc62e6591f
joseRvas13 <142409154+joseRvas13@users.noreply.github.com>
2024-03-07 09:07:02 +0000

    cambios para el login y logout

--- Aplicaciones/Recetarium/urls.py
+++ Aplicaciones/Recetarium/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('crear-elegir-receta/', views.crear_elegir_receta),
-    path('recuperacion-de-contraseña/', views.olvido_contraseña, name="olvido_contraseña"),
-    path('inicio-sesion-registro/', views.inicio_sesion_registro, name="inicio_sesion_registro"),
-    path('salud-nutricion/', views.salud_nutricion, name="salud_nutricion"),
-    path('calculadora-salud/', views.calculadora_salud, name="calculadora_salud"),
+    path('index/', views.index, name='index'),
+    path('crear-elegir-receta/', views.crear_elegir_receta, name='crear_elegir_receta'),
+    path('recuperacion-de-contraseña/', views.olvido_contraseña, name='olvido_contraseña'),
+    path('inicio-sesion-registro/', views.inicio_sesion_registro, name='inicio_sesion_registro'),
+    path('salud-nutricion/', views.salud_nutricion, name='salud_nutricion'),
+    path('calculadora-salud/', views.calculadora_salud, name='calculadora_salud'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    
+    # Agrega aquí otras URLs según sea necesario
 ]
--- Aplicaciones/Recetarium/views.py
+++ Aplicaciones/Recetarium/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
+    # Your view logic here
     return render(request, 'index.html')
 
 def crear_elegir_receta(request):
@@ -19,3 +19,19 @@ def salud_nutricion(request):
 
 def calculadora_salud(request):
     return render(request, 'Calculadora_De_Salud.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Cambia 'home' por el nombre de tu vista de inicio
+        else:
+            return render(request, 'login.html', {'error_message': 'Usuario o contraseña incorrectos'})
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Cambia 'login' por el nombre de tu vista de login
