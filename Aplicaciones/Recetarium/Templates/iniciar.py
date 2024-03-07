93fe19535d4960907e60d3850c513478418d3e3e
joseRvas13 <142409154+joseRvas13@users.noreply.github.com>
2024-03-07 05:48:11 +0000

    arechivo iniciar ¿. py no completado pero en proceso para ensayar

---
+++ Aplicaciones/Recetarium/Templates/iniciar.py
@@ -0,0 +1,5 @@
+import mysql.connector 
+
+conexion = mysql.connector.connect(user='root', password='', host='locahost',database='recetariumv7alpha'
+                                    port
+                                    )
